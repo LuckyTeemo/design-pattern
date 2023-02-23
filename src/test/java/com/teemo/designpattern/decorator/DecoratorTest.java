d85503b63a53467934b2d4d5fb76939995e0950c
LuckyTeemo <269188607@qq.com>
2023-02-23 22:13:26 +0800

    装饰模式

--- src/test/java/com/teemo/designpattern/decorator/DecoratorTest.java
+++ src/test/java/com/teemo/designpattern/decorator/DecoratorTest.java
@@ -10,7 +10,7 @@ public class DecoratorTest {
     public void test() {
 
         // 使用普通地查询天气方法
-        WeatherServiceImpl weatherService = new WeatherServiceImpl();
+        WeatherService weatherService = new WeatherServiceImpl();
         System.out.println(weatherService.queryWeather("2"));
 
         // 使用只查询城市的装饰类查询天气
